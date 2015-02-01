5e55377d5b4f6218be7378b73bbc9f6b5a9377c1
alessandrorusso <alessandro.russo80@gmail.com>
2015-02-01 13:35:11 +0100

    Inseriti campi aggiunti profilo autoscuola (patente, auto, moto, ecc...) Traduzione in italiano del modulo per le prenotazioni

---
+++ administrator/components/com_users/scripts/profileautoscuola.js
@@ -0,0 +1,49 @@
+window.addEvent('domready',function(){
+    
+        jQuery('#jform_profileautoscuola_car').parent().parent().hide();
+        jQuery('#jform_profileautoscuola_bike').parent().parent().hide();
+        jQuery('#jform_profileautoscuola_truck').parent().parent().hide();
+	
+	jQuery('#jform_profileautoscuola_license').change(function(){
+            
+            switch (jQuery('#jform_profileautoscuola_license' ).val()){
+                case 'A':
+                    jQuery('#jform_profileautoscuola_bike').parent().parent().show();
+                    resetTransport('#jform_profileautoscuola_car');
+                    resetTransport('#jform_profileautoscuola_truck');
+                    break;
+                
+                case 'AM':
+                    jQuery('#jform_profileautoscuola_bike').parent().parent().show();
+                    resetTransport('#jform_profileautoscuola_car');
+                    resetTransport('#jform_profileautoscuola_truck');
+                    break;
+                    
+                case 'B':
+                    jQuery('#jform_profileautoscuola_car').parent().parent().show();
+                    resetTransport('#jform_profileautoscuola_bike');
+                    resetTransport('#jform_profileautoscuola_truck');
+                    break;
+                
+                case 'C':
+                    jQuery('#jform_profileautoscuola_truck').parent().parent().show();
+                    resetTransport('#jform_profileautoscuola_bike');
+                    resetTransport('#jform_profileautoscuola_car');
+                    break;
+                
+                default:
+                    resetTransport('#jform_profileautoscuola_car');
+                    resetTransport('#jform_profileautoscuola_bike');
+                    resetTransport('#jform_profileautoscuola_truck');
+            }            
+        });
+})
+
+function resetTransport(id){
+    jQuery(id).val('');
+    jQuery(id).parent().parent().hide();    
+}
+
+
+
+
