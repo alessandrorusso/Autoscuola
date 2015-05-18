2442b983a2c1bed8471bc73cd2a9099b6f5dd5f3
alessandrorusso <alessandro.russo80@gmail.com>
2015-05-18 17:39:32 +0200

    fix responsive

--- templates/as002044free/index.php
+++ templates/as002044free/index.php
@@ -192,12 +192,12 @@ include ('includes/includes.php');
         <div id="header-bottom-row" style="width: 91%;">
             <div class="container" >
                 <div class="row" style="margin-top: 5%; margin-bottom: 5%;">
-                    <div class="col-xs-4 col-md-5">
+                    <div class="col-xs-8 col-md-6 col-lg-5">
                         <?php if ($this->countModules('as-position-7')): ?>
                             <jdoc:include type="modules" name="as-position-7" style="themeHtml5" />
                         <?php endif; ?>
                     </div>
-                    <div class="col-xs-8 col-md-7">
+                    <div class="col-xs-4 col-md-6 col-lg-7">
                         <?php if ($this->countModules('as-position-23')): ?>
                             <jdoc:include type="modules" name="as-position-23" style="themeHtml5" />
                         <?php endif; ?>
@@ -209,7 +209,7 @@ include ('includes/includes.php');
         <div id="header-bottom-row" style="width: 91%;">
             <div class="container" >
                 <div class="row" style="margin-top: 5%; margin-bottom: 5%">
-                    <div class="col-xs-12 col-md-12">
+                    <div class="col-xs-12 col-md-12 col-lg-12">
                         <?php if ($this->countModules('as-position-7')): ?>
                             <jdoc:include type="modules" name="as-position-7" style="themeHtml5" />
                         <?php endif; ?>
@@ -221,7 +221,7 @@ include ('includes/includes.php');
         <div id="header-bottom-row" style="width: 91%;">
             <div class="container" >
                 <div class="row" style="margin-top: 5%; margin-bottom: 5%">
-                    <div class="col-xs-12 col-md-12">
+                    <div class="col-xs-12 col-md-12 col-lg-12">
                         <?php if ($this->countModules('as-position-23')): ?>
                             <jdoc:include type="modules" name="as-position-23" style="themeHtml5" />
                         <?php endif; ?>
